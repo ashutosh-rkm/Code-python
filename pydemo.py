4ded0d0af96d2604b6964691bd4723a19f4d6382
ashutosh-rkm <ashutosh.rkm@gmail.com>
2024-01-08 12:43:27 +0530

    This is pythons code

---
+++ pydemo.py
@@ -0,0 +1,7 @@
+num=int(input("Enter any number\n"))
+if num<0:
+  print(f"Type number {num} is negative")
+elif num==0:
+  print("Type number is zero")
+else:
+  print(f"Type number {num} is positive")
